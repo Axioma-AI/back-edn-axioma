ff4f0c902c05b4490248d997caad51a2f88d5cfb
ElJoamy <josephsalguero1@upb.edu>
2025-01-12 19:38:56 -0400

    refactor: add new fields to NewsTranslationModel and NewsModel for enhanced content representation

--- src/models/news_tag_model.py
+++ src/models/news_tag_model.py
@@ -33,6 +33,21 @@ class NewsTranslationModel(Base):
     title_tra = Column(Text, nullable=False)
     detail_tra = Column(Text, nullable=False)
     content_tra = Column(Text, nullable=False)
+    summary_tra = Column(Text, nullable=True)
+    justification_tra = Column(Text, nullable=True)
+    news_type_category_tra = Column(String(50), nullable=True)
+    news_type_justification_tra = Column(Text, nullable=True)
+    purpose_objective_tra = Column(String(50), nullable=True)
+    purpose_audience_tra = Column(String(50), nullable=True)
+    context_temporality_tra = Column(String(50), nullable=True)
+    context_location_tra = Column(String(255), nullable=True)
+    content_facts_vs_opinions_tra = Column(String(50), nullable=True)
+    content_precision_tra = Column(String(50), nullable=True)
+    content_impartiality_tra = Column(String(50), nullable=True)
+    structure_clarity_tra = Column(String(50), nullable=True)
+    structure_key_data_tra = Column(String(50), nullable=True)
+    tone_neutrality_tra = Column(String(50), nullable=True)
+    tone_ethics_tra = Column(String(50), nullable=True)
     language = Column(Enum('en', 'es'), nullable=False)
 
     news = relationship('NewsModel', back_populates='translations')
@@ -46,17 +61,33 @@ class NewsModel(Base):
     detail = Column(Text, nullable=True)
     image_url = Column(Text, nullable=True)
     content = Column(Text, nullable=True)
+    summary = Column(Text, nullable=True)
     author = Column(String(255), nullable=True)
     publish_datetime = Column(DateTime, nullable=True)
     location = Column(String(255), nullable=True)
     source_link = Column(String(255), nullable=False, unique=True)
     sentiment_category = Column(Enum(SentimentCategory), nullable=False)
+    justification = Column(Text)
     sentiment_score = Column(Numeric(5, 5), nullable=False)
+    news_type_category = Column(String(50), nullable=True)
+    news_type_justification = Column(Text, nullable=True)
+    purpose_objective = Column(String(50), nullable=True)
+    purpose_audience = Column(String(50), nullable=True)
+    context_temporality = Column(String(50), nullable=True)
+    context_location = Column(String(255), nullable=True)
+    content_facts_vs_opinions = Column(String(50), nullable=True)
+    content_precision = Column(String(50), nullable=True)
+    content_impartiality = Column(String(50), nullable=True)
+    structure_clarity = Column(String(50), nullable=True)
+    structure_key_data = Column(String(50), nullable=True)
+    tone_neutrality = Column(String(50), nullable=True)
+    tone_ethics = Column(String(50), nullable=True)
 
     tag_associations = relationship("NewsTagAssociation", back_populates="news", overlaps="tags")
     tags = relationship("TagsModel", secondary='news_tag', back_populates="news", overlaps="tag_associations", viewonly=True)
     favorites = relationship("FavoritesModel", back_populates="news", cascade="all, delete-orphan")
     translations = relationship('NewsTranslationModel', back_populates='news')
+    characters = relationship('NewsCharactersModel', back_populates='news')
 
     @staticmethod
     def from_bank_new(bank_new: BankNew) -> 'NewsModel':
@@ -73,3 +104,23 @@ class NewsModel(Base):
             sentiment_category=bank_new.sentiment_category,
             sentiment_score=bank_new.sentiment_score,
         )
+
+class NewsCharactersModel(Base):
+    __tablename__ = 'news_characters'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    news_id = Column(Integer, ForeignKey('news.id'), nullable=False)
+    character_name = Column(String(255, collation='utf8mb4_unicode_ci'), nullable=False)
+    character_description = Column(Text(collation='utf8mb4_unicode_ci'), nullable=False)
+
+    news = relationship('NewsModel', back_populates='characters')
+
+class NewsTransCharactersModel(Base):
+    __tablename__ = 'news_trans_characters'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    news_characters_id = Column(Integer, ForeignKey('news_characters.id'), nullable=False)
+    character_description_tra = Column(Text, nullable=False)
+    language = Column(Enum('en', 'es'), nullable=False)
+
+    news_character = relationship('NewsCharactersModel')
